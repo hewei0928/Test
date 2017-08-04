9fa721807b1b368caa5978a7ce0ea8b259e93578
hewei0928 <1476276623@qq.com>
2017-08-04 14:18:58 +0800

    2017年8月4日14:18:54

---
+++ src/com/algorithm/leetcode/TreeNode.java
@@ -0,0 +1,13 @@
+package com.algorithm.leetcode;
+
+/**
+ * Created by Administrator
+ * on 2017/8/4 14:15.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
