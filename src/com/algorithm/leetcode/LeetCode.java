9b8f0ee525767a83aec83496c37418c20297f8a0
hewei0928 <1476276623@qq.com>
2017-08-04 14:40:05 +0800

    2017年8月4日14:40:03

--- src/com/algorithm/leetcode/LeetCode.java
+++ src/com/algorithm/leetcode/LeetCode.java
@@ -1142,6 +1142,7 @@ public class LeetCode {
     }
 
 
+    //二叉树相关知识， 递归思想。
     /**
      * 617. Merge Two Binary Trees
      * 二叉树合并
@@ -1150,8 +1151,30 @@ public class LeetCode {
      * @return
      */
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        TreeNode result = new TreeNode();
-        return result;
+        if(t1 == null && t2 == null){
+            return null;
+        } else if(t1 == null){
+            return t2;
+        } else if(t2 == null){
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
+
+    //待做
+    /**
+     * 28. Implement strStr()
+     * 实现类java String。indexOf方法
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
     }
 
 
